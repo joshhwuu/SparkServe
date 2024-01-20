db3124c4fb996a712f98bd887728c63f4b9d2664
Jaden Hums <jadenhums51@gmail.com>
2024-01-20 15:12:05 -0800

    add phone screen component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
 import { useEffect } from 'react';
+import Router from './Router';
+
+import React from 'react'
+import { Typography } from '@mui/material';
+import { Container } from '@mui/material';
 
 function App() {
   useEffect(() => {
@@ -20,20 +24,13 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <Container maxWidth="sm">
+        <Typography component="div" style={{ 
+          backgroundColor: 'Orange', height: '100vh', width: '451pt'
+        }}>
+          <Router />
+        </Typography>
+      </Container>
     </div>
   );
 }
---
+++ src/Router.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+function Router() {
+  return (
+    <div>HELLO WORLD</div>
+  )
+}
+
+export default Router;
