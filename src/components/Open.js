85a21d60d014ffcca86e4b858b49fa7be4340b99
josh <jwu96@students.cs.ubc.ca>
2024-01-20 20:45:21 -0800

    goat

--- src/components/Open.js
+++ src/components/Open.js
@@ -15,13 +15,73 @@ function Open() {
                 gap: 30,
             }}
         >
-            <LookingFor />
+            <FindOpen />
         </div>
     );
-  }
+}
 
-  function Open() {
-    
-  }
-  
-  export default Open;
+function FindOpen() {
+    const [selectedButton, setSelectedButton] = useState([]);
+
+    const buttons = ['Arts & Culture', 'Community Service', 'Environment', 'Food', 'Photography',
+        'Sports', 'Enterntainment', 'Music', 'Technology', 'Health & Wellness', 'Education'];
+
+    const handleButtonClick = (index) => {
+        if (selectedButton.includes(index)) {
+            setSelectedButton(selectedButton.filter((button) => button !== index));
+        } else {
+            setSelectedButton([...selectedButton, index]);
+        }
+    }
+
+    return (
+        <div>
+            <StyledFindOpen>Open to roles in...</StyledFindOpen>
+            <ButtonContainer>
+                {buttons.map((button, index) => (
+                    <Button
+                        key={index}
+                        variant={selectedButton.includes(index) ? 'contained' : 'outlined'}
+                        onClick={() => handleButtonClick(index)}
+                        style={{
+                            marginRight: '0px',
+                            color: '#E9E9E9',
+                            borderRadius: '15px'
+                        }}
+                        size="small"
+                    >
+                        {button}
+                    </Button>
+                ))}
+            </ButtonContainer>
+            <ContinueButton variant="contained" color="primary" LinkComponent={Link} to="/open" endIcon={<ArrowRightAltIcon />}>
+                Continue
+            </ContinueButton>
+            
+        </div>
+    );
+}
+
+const StyledFindOpen = styled.h1`
+color: #E9E9E9;
+font-family: Lexend;
+font-size: 40.218px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`;
+
+const ButtonContainer = styled.div`
+margin-top: 20px;
+margin-right: 16px;
+display: flex;
+flex-wrap: wrap;
+gap: 8px;
+margin-left: 50px
+`;
+
+const ContinueButton = styled(Button)`
+margin-top: 10px;
+display: inline-block;
+`;
+export default Open;
