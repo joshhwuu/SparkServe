9510ce27412b7d61b90292f7e358feb8db5cb396
joshhwuu <124658199+joshhwuu@users.noreply.github.com>
2024-01-20 20:07:05 -0800

    made looking branch and open branch (#1)
    Co-authored-by: josh <jwu96@students.cs.ubc.ca>

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import { createGlobalStyle } from 'styled-components'
 import Main from './containers/Main'
 import Message from './containers/Message'
 import Login from './containers/Login';
+import Looking from './components/Looking';
+import Open from './components/Open';
 import ProfilePage from './components/UserProfile';
+
 
 import React from 'react'
 import { Typography } from '@mui/material';
@@ -38,6 +41,8 @@ function App() {
             <Route path="profile" element={<ProfilePage />} />
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/looking" element={<Looking />} />
+            <Route path="/open" element={<Open />} />
           </Routes>
         </Typography>
       </Container>
